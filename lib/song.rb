bd412563ee3099abf662af98c99e3c13539f3058
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-09 14:22:37 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @count = 0
+  end
+  
+  def self.count
+    @count
+  end
+end
